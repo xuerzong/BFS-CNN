8d0bc693d22b7cef62ce9d55e32c7252d5877aa9
xucong_sx <xucong_sx@qiyi.com>
2021-03-21 20:42:51 +0800

    create bgs data

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,10 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+import matplotlib.pyplot as plt
+
+array = np.ndarray
+LEN = 151
 
 
 class BGSDynamicDataSet(Dataset):
@@ -42,7 +46,7 @@ class BGSFixedDataSet(Dataset):
         pass
 
 
-def create_data(n):
+def create_data(n: int):
     """
     模拟生成数据
     :param n: n条轨道的BGS组成一个数据
@@ -50,25 +54,72 @@ def create_data(n):
             分别对应BGS 和 BFS
     """
     # 思路：随机模拟BFS信号，生成对应的BGS
-    # 1. 随机生成一组BFS， 形状[N]
-    bfs = np.random.uniform(0.05, 0.95, n)
-    # 2. TODO 每一个BFS生成对应的BGS, 形状[151, N]
-    bgs = np.zeros((151, n), dtype=float)
-    # 3. TODO 添加白噪声
-    # 4。TODO 根据频率进行归一化
+    bfs = np.random.uniform(0.05, 0.95, n) * LEN
+    # bgs = np.zeros((LEN, n), dtype=float)
+    tmp = np.zeros((n, LEN), dtype=array)
+    for i in range(n):
+        line_width = np.random.uniform(0.1, 0.5) * LEN
+        peak_gain = 10
+        _bfs = bfs[i]
+        tmp[i] = create_bgs(data=tmp[i], peak_gain=peak_gain, line_width=line_width, bfs=_bfs)
+        tmp[i] = awgn(tmp[i])
+        tmp[i] = frequency_normalization(tmp[i])
+    bgs = tmp.T
+    # return bgs
     return torch.tensor([bgs], dtype=torch.float), torch.tensor(bfs, dtype=torch.float)
 
 
+def create_bgs(data: array, peak_gain: float, line_width: float, bfs: float) -> array:
+    y = np.array([])
+    for item in range(len(data)):
+        tmp = peak_gain / (1 + np.square((item - bfs) / (line_width / 2)))
+        y = np.append(y, tmp)
+    return y
+
+
+def awgn(data: array) -> array:
+    """
+    加入高斯白噪声 Additive White Gaussian Noise
+    :param data: 原始信号
+    :return: 加入噪声后的信号
+    """
+    snr = np.random.uniform(5, 20)
+    _signal = np.sum(data ** 2) / len(data)
+    _noise = _signal / 10 ** (snr / 10)
+
+    noise = np.random.randn(len(data)) * np.sqrt(_noise)
+    return data + noise
+
+
+def frequency_normalization(data: array) -> array:
+    """
+    BFS 归一化处理
+    :param data: 原始数据
+    :return: 处理后的数据
+    """
+    _min = np.min(data)
+    _max = np.max(data)
+
+    return (data - _min) / (_max - _min)
+
+
 if __name__ == '__main__':
-    dataset = BGSDynamicDataSet()
-    loader = DataLoader(dataset=dataset,
-                        shuffle=False,
-                        batch_size=1,
-                        num_workers=2
-                        )
-    for i, data in enumerate(loader):
-        print(data)
-        break
+    a = create_data(200)
+    # a = a.T
+    print(len(a))
+    for i in range(80):
+        plt.plot(np.arange(len(a[i])), a[i])
+    plt.ylim(-0.1, 1.1)
+    plt.show()
+    # dataset = BGSDynamicDataSet()
+    # loader = DataLoader(dataset=dataset,
+    #                     shuffle=False,
+    #                     batch_size=1,
+    #                     num_workers=2
+    #                     )
+    # for i, data in enumerate(loader):
+    #     print(data)
+    #     break
         # if i % 20 == 0:
         #     print(data)
         #     print('{}/{}'.format(i * 3, len(dataset)))
