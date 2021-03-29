67a7ba171ca669a5ae046eb0ffe92254da3bbc6b
xuerzong <xuerzong@gmail.com>
2021-03-30 00:50:56 +0800

    update train

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+
 import torch
 from torch import nn
 from torch.nn import MSELoss
@@ -8,9 +9,8 @@ from torch.utils.data import DataLoader
 from src.dataset import BGSDynamicDataSet
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 from src.models import BfsCNN
-from src.utils import create_csv, write_csv
+from src.utils import write_csv
 
 array = np.ndarray
 
@@ -34,7 +34,6 @@ def train(loader, model, device, loss_fn, op) -> array:
         op.zero_grad()
         loss.backward()
         op.step()
-
         if i % 10 == 0 or i == len(loader) - 1:
             print("Training {:5d} / {:5d}, loss: {:10.5f}"
                   .format(i + 1, len(loader), train_loss / (i + 1)))
@@ -47,20 +46,20 @@ def test(loader, model, device, loss_fn) -> array:
     test_loss = 0.0
 
     loss_arr = np.array([], dtype=float)
+    with torch.no_grad():
+        for i, x in enumerate(loader):
+            x, target = x
+            x, target = x.to(device), target.to(device)
+            output = model(x)
+            output = output.view(output.size(0), -1)
+            loss = loss_fn(output, target)
+            test_loss += loss
 
-    for i, x in enumerate(loader):
-        x, target = x
-        x, target = x.to(device), target.to(device)
-        output = model(x)
-        output = output.view(output.size(0), -1)
-        loss = loss_fn(output, target)
-        test_loss += loss
+            loss_arr = np.append(loss_arr, float(loss))
 
-        loss_arr = np.append(loss_arr, float(loss))
-
-        if i % 10 == 0 or i == len(loader) - 1:
-            print("Testing {:5d} / {:5d}, loss: {:10.5f}"
-                  .format(i + 1, len(loader), test_loss / (i + 1)))
+            if i % 10 == 0 or i == len(loader) - 1:
+                print("Testing {:5d} / {:5d}, loss: {:10.5f}"
+                    .format(i + 1, len(loader), test_loss / (i + 1)))
 
     return loss_arr
 
@@ -71,19 +70,20 @@ if __name__ == '__main__':
         model = torch.load('model.pkl')
     else:
         model = BfsCNN()
-
-    batch_size = 2**4
-    learn_rate = 0.05
-    train_csv_name = f'train_{batch_size}_{learn_rate}'
-    test_csv_name = f'test_{batch_size}_{learn_rate}'
+    N = 128
+    batch_size = 4
+    learn_rate = 0.001
+    epoch = 22
+    train_csv_name = f'x_train_{batch_size}_{learn_rate}'
+    test_csv_name = f'x_test_{batch_size}_{learn_rate}'
 
     optimizer = Adam(model.parameters(), lr=learn_rate)
     scheduler = StepLR(optimizer, step_size=1, gamma=0.9)
     loss_fn = MSELoss()
-    dataset = BGSDynamicDataSet(size=2048)
+    dataset = BGSDynamicDataSet(size=375, n=N)
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    dataset = BGSDynamicDataSet(size=128, n=N)
     test_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4)
-    epoch = 30
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
 
@@ -91,23 +91,14 @@ if __name__ == '__main__':
         print("Epoch {} :".format(i + 1))
 
         train_loss_list_tmp = train(train_loader, model, device, loss_fn, optimizer)
+        torch.cuda.empty_cache()
         test_loss_list_tmp = test(test_loader, model, device, loss_fn)
+        torch.cuda.empty_cache()
 
         scheduler.step()
 
         write_csv(filename=train_csv_name, flag=False, data=train_loss_list_tmp)
         write_csv(filename=test_csv_name, flag=False, data=test_loss_list_tmp)
 
-        filename = f'results/{batch_size}_{learn_rate}_{i}.png'
-        x = np.arange(len(train_loss_list_tmp))
-        plt.plot(x, train_loss_list_tmp, label='train_loss')
-        plt.plot(x, test_loss_list_tmp, label='test_loss')
-        plt.xlabel('x')
-        plt.ylabel('loss')
-        # plt.ylim(0, 1.5)
-        plt.legend()  # 图例
-        plt.title(filename)
-        plt.savefig(filename)
-        plt.show()
-    print('---')
-    torch.save(model, 'model.pkl')
+        torch.save(model, 'model_x.pkl')
+
