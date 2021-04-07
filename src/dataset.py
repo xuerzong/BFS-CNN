7ea2206989843ff49a6c2407ee69a50c85eb3c6e
xuerzong <xuerzong@gmail.com>
2021-04-07 14:17:27 +0800

    update dataset

--- src/dataset.py
+++ src/dataset.py
@@ -3,17 +3,12 @@ from torch.utils.data import Dataset
 from torch.tensor import Tensor
 import numpy as np
 from typing import Tuple
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 array = np.ndarray
 LEN = 151
 
-bfsMin = 10.85 # GHz  截取最小频率
-bfsMax = 10.95 # GHz  截取最大频率
-bfsRange = bfsMax - bfsMin
-
-swMin = 0.02 # GHz
-swMax = 0.06 # GHz
-swRange = swMax - swMin
 
 class BGSDynamicDataSet(Dataset):
     """
@@ -39,44 +34,65 @@ class BGSDynamicDataSet(Dataset):
         return self.size
 
 
-def create_data(n: int) -> Tuple[Tensor, Tensor, Tensor]:
-    bfs = np.random.uniform(bfsMin + bfsRange*0.05, bfsMax - bfsRange*0.05, n)
-    sw = np.random.uniform(swMin + swRange * 0.01, swMax - swRange * 0.05, n)
-    tmp = np.zeros((n, LEN), dtype=array)
+def create_data(
+    n: int
+) -> Tuple[Tensor, Tensor, Tensor]:
 
-    bfs, sw = normalization(bfs=bfs, sw=sw)
+    bgs = np.zeros((n, LEN), dtype=array)
+
+    bfs, sw = normalization(
+        bfs=get_bfs(n=n),
+        sw=get_sw(n=n)
+    )
+
     for i in range(n):
         peak_gain = 1
-        tmp[i] = create_bgs(
-            data=tmp[i],
+        bgs[i] = create_bgs(
             peak_gain=peak_gain,
             sw=sw[i],
             bfs=bfs[i]
         )  # 生成理想BGS
         # tmp[i] = awgn(tmp[i])  # 添加白噪声
-    bgs = tmp.T
-    return tmp
-    bgs = bgs / np.max(bgs)
-    bfs, sw = normalization(bfs, sw)
+    bgs = bgs.T
+    return bgs
     return torch.tensor([bgs], dtype=torch.float), \
            torch.tensor(bfs, dtype=torch.float), \
            torch.tensor(sw, dtype=torch.float)
 
 
 def create_bgs(
-        data: array,
-        peak_gain: float,
-        sw: float,
-        bfs: float
+    peak_gain: float,
+    sw: float,
+    bfs: float
 ) -> array:
-    y = np.array([])
-    for item in np.linspace(0, 1, len(data)):
+    res = np.array([])
+    for item in np.linspace(0, 1, LEN):
         tmp = peak_gain / (1 + np.square((item - bfs) / (sw / 2)))
-        y = np.append(y, tmp)
-    return y
+        res = np.append(res, tmp)
+    return res
 
 
-def awgn(data: array) -> array:
+def get_bfs(
+    n: int
+) -> array:
+    bfsMin = 10.85
+    bfsMax = 10.95
+    bfsRange = bfsMax - bfsMin
+    return np.random.uniform(bfsMin + bfsRange*0.05, bfsMax - bfsRange*0.05, n)
+
+
+def get_sw(
+    n: int
+) -> array:
+    swMin = 0.02
+    swMax = 0.06
+    swRange = swMax - swMin
+    return np.random.uniform(swMin + swRange * 0.01, swMax - swRange * 0.05, n)
+
+
+def awgn(
+    data: array
+) -> array:
     snr = np.random.uniform(5, 20)
     _signal = np.sum(data ** 2) / len(data)
     _noise = _signal / 10 ** (snr / 10)
@@ -85,9 +101,15 @@ def awgn(data: array) -> array:
     return data + noise
 
 
-def normalization(bfs: array, sw: array) -> Tuple[array, array]:
-    return (bfs - bfsMin) / bfsRange, sw / bfsRange
+def normalization(
+    bfs: array,
+    sw: array
+) -> Tuple[array, array]:
+    bfsMin = np.min(bfs)
+    bfsMax = np.max(bfs)
+    return (bfs - bfsMin) / (bfsMax - bfsMin), sw / (bfsMax - bfsMin)
 
 
 if __name__ == '__main__':
     pass
+
