79e86effc08dfca241d46e3fde80fe74dd1a1d87
xuerzong <xuerzong@gmail.com>
2021-05-12 20:13:15 +0800

    fix: xxx

--- src/train.py
+++ src/train.py
@@ -15,7 +15,7 @@ from src.utils import write_csv
 array = np.ndarray
 
 
-def train(loader, model, device, loss_fn, op) -> array:
+def train(loader, model, device, loss_fn, op, mean_loss) -> float:
     model.train()
     train_loss = 0.0
 
@@ -31,14 +31,30 @@ def train(loader, model, device, loss_fn, op) -> array:
         loss_arr = np.append(loss_arr, float(loss))
 
         train_loss += loss
-        op.zero_grad()
-        loss.backward()
-        op.step()
+
+        if mean_loss == 0:
+            print(1)
+            op.zero_grad()
+            loss.backward()
+            op.step()
+            train_loss += loss
+        elif mean_loss >= float(loss):
+            print(2)
+            op.zero_grad()
+            loss.backward()
+            op.step()
+            train_loss += loss
+        else:
+            print(3)
+            train_loss += mean_loss
+        
+        print(float(loss))
+
         if i % 10 == 0 or i == len(loader) - 1:
             print("Training {:5d} / {:5d}, loss: {:10.5f}"
                   .format(i + 1, len(loader), train_loss / (i + 1)))
 
-    return loss_arr
+    return train_loss / (i + 1)
 
 
 def test(loader, model, device, loss_fn) -> array:
@@ -65,22 +81,24 @@ def test(loader, model, device, loss_fn) -> array:
 
 
 if __name__ == '__main__':
-    # model: nn.Module
-    # if os.path.exists('model.pkl'):
-    #     model = torch.load('model.pkl')
-    # else:
-    #     model = BfsCNN()
+    model: nn.Module
+    if os.path.exists('model.pkl'):
+        model = torch.load('model.pkl')
+    else:
+        model = BfsCNN()
     model = BfsCNN()
     N = 224
-    batch_size = 16
+    batch_size = 8
     learn_rate = 0.001
     epoch = 30
+
+    mean_loss = 0
 
 
     optimizer = Adam(model.parameters(), lr=learn_rate)
     scheduler = StepLR(optimizer, step_size=1, gamma=0.9)
     loss_fn = MSELoss()
-    dataset = BGSDynamicDataSet(size=375, n=N)
+    dataset = BGSDynamicDataSet(size=480, n=N)
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4)
     dataset = BGSDynamicDataSet(size=128, n=N)
     test_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4)
@@ -90,7 +108,7 @@ if __name__ == '__main__':
     for i in range(epoch):
         print("Epoch {} :".format(i + 1))
 
-        train_loss_list_tmp = train(train_loader, model, device, loss_fn, optimizer)
+        mean_loss = train(train_loader, model, device, loss_fn, optimizer, mean_loss)
         torch.cuda.empty_cache()
         test_loss_list_tmp = test(test_loader, model, device, loss_fn)
         torch.cuda.empty_cache()
@@ -98,4 +116,7 @@ if __name__ == '__main__':
         scheduler.step()
 
         torch.save(model, 'model.pkl')
+    
+
+    torch.save(model, 'xxx.pkl')
 
